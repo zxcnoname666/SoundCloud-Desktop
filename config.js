b5129509a962ff5f3d9324315fc86a04984c1b1e
fydne <156155219+zxcnoname666@users.noreply.github.com>
2024-01-29 00:21:11 +0500

    1.3.0

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 module.exports = {
     proxy: [
         //'1.1.1.1:80',
-        'soundcloud-proxy.fydne.dev:3128'
     ],
     translations: {
         ru: { // ru, kk, ky, be
             proxy_available_not_found: 'Доступные прокси-серверы не найдены',
             proxy_work_not_found: 'Работающие прокси-серверы не найдены',
-            proxy_connected: 'Подключен к прокси-серверу - [HIDDEN]',
+            proxy_connected: 'Подключен к прокси-серверу - {ip}',
         },
         en: {
             proxy_available_not_found: 'Available proxy servers not found',
             proxy_work_not_found: 'Working proxy servers not found',
-            proxy_connected: 'Connected to proxy server - [HIDDEN]',
+            proxy_connected: 'Connected to proxy server - {ip}',
         },
         /*
         'iso code': {...},
