05a7e7dcab69ca56e942669be31de0694e34acaa
no name <156155219+zxcnoname666@users.noreply.github.com>
2024-11-13 16:38:57 +0500

    1.9.4

--- modules/ProxyManager.js
+++ modules/ProxyManager.js
@@ -133,6 +133,7 @@ module.exports = class ProxyManager {
             'appleid.apple.com;iforgot.apple.com;www.apple.com;appleid.cdn-apple.com;*-ssl.mzstatic.com;appleid.cdn-apple.com' //apple
         );
 
+        /*
         if (await CheckWorkDomain('https://geo.captcha-delivery.com')) {
             bypass_proxy += ';*.captcha-delivery.com' // captcha
         }
@@ -140,6 +141,7 @@ module.exports = class ProxyManager {
         if (await CheckWorkDomain('https://api-auth.soundcloud.com/')) {
             bypass_proxy += ';api-auth.soundcloud.com' // api auth
         }
+        */
 
         const proxyCfg = {
             mode: 'fixed_servers',
