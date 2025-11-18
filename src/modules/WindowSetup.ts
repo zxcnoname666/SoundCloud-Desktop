30df6afff70e66375c5681452c93ad5d8392ae9c
zxcnoname666 <156155219+zxcnoname666@users.noreply.github.com>
2025-11-18 15:45:42 +0500

    chore: add new cloud domain to proxy

--- src/modules/WindowSetup.ts
+++ src/modules/WindowSetup.ts
@@ -1,18 +1,9 @@
-import { join } from 'node:path';
-import { Readable } from 'node:stream';
-import {
-  BrowserWindow,
-  Menu,
-  Tray,
-  app,
-  globalShortcut,
-  nativeImage,
-  protocol,
-  shell,
-} from 'electron';
+import {join} from 'node:path';
+import {Readable} from 'node:stream';
+import {app, BrowserWindow, globalShortcut, Menu, nativeImage, protocol, shell, Tray,} from 'electron';
 import fetch from 'node-fetch';
-import type { WindowBounds } from '../types/config.js';
-import { ProxyManager } from './ProxyManager.js';
+import type {WindowBounds} from '../types/config.js';
+import {ProxyManager} from './ProxyManager.js';
 
 export class WindowSetup {
   private static tray: Tray | null = null;
@@ -344,16 +335,22 @@ export class WindowSetup {
     );
   }
 
-  private static shouldProxyDomain(hostname: string): boolean {
-    const proxyDomains = [
-      'soundcloud.com',
-      'sndcdn.com',
-      'api.soundcloud.com',
-      'api-v2.soundcloud.com',
-    ];
+    private static shouldProxyDomain(hostname: string): boolean {
+        /*
+         const proxyDomains = [
+           'soundcloud.com',
+           'sndcdn.com',
+           'api.soundcloud.com',
+           'api-v2.soundcloud.com',
+             'soundcloud.cloud'
+         ];
 
-    return proxyDomains.some((domain) => hostname === domain || hostname.endsWith(`.${domain}`));
-  }
+         return proxyDomains.some((domain) => hostname === domain || hostname.endsWith(`.${domain}`));
+         */
+
+        console.debug("shouldProxyDomain.hostname", hostname)
+        return true;
+    }
 
   private static async getProxyResponse(request: Request): Promise<Response> {
     const proxyManager = ProxyManager.getInstance();
