bb576812bd7023558d57802901816f1be1043f09
zxcnoname666 <156155219+zxcnoname666@users.noreply.github.com>
2025-11-27 15:07:10 +0500

    ci: try fix macos build !release: major

--- scripts/forge-make.cjs
+++ scripts/forge-make.cjs
@@ -5,14 +5,16 @@ const path = require('path');
 
 // Change to dist directory and run electron-forge make
 const distDir = path.join(__dirname, '..', 'dist');
+const forgeBin = path.join(__dirname, '..', 'node_modules', '.bin', 'electron-forge');
 
 // Pass all arguments after script name to electron-forge
 const args = ['make', ...process.argv.slice(2)];
 
 console.info('Running electron-forge with args:', args);
 console.info('Working directory:', distDir);
+console.info('Forge binary:', forgeBin);
 
-const result = spawnSync('electron-forge', args, {
+const result = spawnSync(forgeBin, args, {
     cwd: distDir,
     stdio: 'inherit',
     shell: true,
